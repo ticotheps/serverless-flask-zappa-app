1e63a84120292ae1f25835a4d83e31a2b1529593
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-04 00:28:50 -0500

    Updates returned message in 'my_app.py'

--- my_app.py
+++ my_app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, world!", 200
+    return "Hello, world! Welcome to Tico's FIRST-ever serverless microservice! Today's date/time is 03/04/2020 @ 12:28AM.", 200
 
 # We only need this for local development
 if __name__ == '__main__':
